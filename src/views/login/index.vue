166498c649cb01a98aaac11c18364c8d1f06942a
曾逸超 <zengyichao@digibms.com>
2022-11-25 18:01:27 +0800

    feat((login)): 添加登录校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,28 +1,40 @@
 <template>
   <div class="login-container">
-    <el-form class="login-form">
+    <el-form :model="loginForm" :rules="loginRules" class="login-form">
       <div class="title-container">
         <h3 class="title">用户登录</h3>
       </div>
       <!-- username -->
-      <el-form-item>
+      <el-form-item props="username">
         <span class="svg-container">
           <!-- <svg-icon icon="https://res.lgdsunday.club/user.svg" /> -->
           <svg-icon icon="user" />
         </span>
-        <el-input placeholder="username" name="username" type="text" />
+        <el-input
+          v-model="loginForm.username"
+          placeholder="username"
+          name="username"
+          type="text"
+        />
       </el-form-item>
       <!-- password -->
-      <el-form-item>
+      <el-form-item prop="password">
         <span class="svg-container">
           <el-icon>
             <svg-icon icon="password" />
           </el-icon>
         </span>
-        <el-input placeholder="password" name="password" type="text" />
-        <span class="show-pwd">
+        <el-input
+          v-model="loginForm.password"
+          placeholder="password"
+          :type="passwordType"
+          name="password"
+        />
+        <span class="show-pwd" @click="togglePwdType">
           <el-icon>
-            <svg-icon icon="eye" />
+            <svg-icon
+              :icon="passwordType === 'password' ? 'eye' : 'eye-open'"
+            />
           </el-icon>
         </span>
       </el-form-item>
@@ -36,84 +48,107 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import { validatePassword } from './rules'
 
+// 数据源
+const loginForm = ref({
+  username: 'super-admin',
+  password: '123456'
+})
+
+// 验证规则
+const loginRules = ref({
+  username: [{ required: true, trigger: 'blur', message: '用户名为必填项' }],
+  password: [{ required: true, trigger: 'blur', validator: validatePassword }]
+})
+
+// 密码框文本显示
+const passwordType = ref('password')
+const togglePwdType = () => {
+  if (passwordType.value === 'password') {
+    passwordType.value = 'text'
+  } else {
+    passwordType.value = 'password'
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-  $bg: #2f3a4b;
-  $dark-gray: #889aa4;
-  $light-gray: #eee;
-  $cursor: #FFF;
+$bg: #2f3a4b;
+$dark-gray: #889aa4;
+$light-gray: #eee;
+$cursor: #fff;
 
-  .login-container {
-    min-height: 100%;
-    width: 100%;
-    background-color: $bg;
+.login-container {
+  min-height: 100%;
+  width: 100%;
+  background-color: $bg;
+  overflow: hidden;
+
+  .login-form {
+    margin: 0 auto;
+    padding: 160px 35px 0;
+    position: relative;
+    width: 520px;
     overflow: hidden;
 
-    .login-form {
-      margin: 0 auto;
-      padding: 160px 35px 0;
-      position: relative;
-      width: 520px;
-      overflow: hidden;
-
-      :deep .el-form-item {
-        border: 1px solid rgba(255, 255, 255, .1);
-        background: rgba(0, 0, 0, .1);
-        border-radius: 5px;
-        color: #454545;
-      }
-
-      :deep .el-input {
-        display: inline-block;
-        height: 47px;
-        width: 85%;
-
-        &__wrapper {
-          margin-top: 6px;
-          background: transparent;
-          box-shadow: none;
-        }
-
-        input {
-          padding: 12px 5px 12px 15px;
-          background: transparent;
-          border: none;
-          border-radius: 0;
-          caret-color: $cursor;
-          color: $light-gray;
-        }
-      }
+    :deep .el-form-item {
+      border: 1px solid rgba(255, 255, 255, 0.1);
+      background: rgba(0, 0, 0, 0.1);
+      border-radius: 5px;
+      color: #454545;
     }
 
-    .svg-container {
+    :deep .el-input {
       display: inline-block;
-      padding: 6px 5px 6px 15px;
-      vertical-align: middle;
-      color: $dark-gray;
-    }
+      height: 47px;
+      width: 85%;
 
-    .title-container {
-      position: relative;
+      &__wrapper {
+        margin-top: 6px;
+        background: transparent;
+        box-shadow: none;
+      }
 
-      .title {
-        margin: 0 auto 40px auto;
-        font-size: 26px;
-        text-align: center;
-        font-weight: bold;
+      input {
+        padding: 12px 5px 12px 15px;
+        background: transparent;
+        border: none;
+        border-radius: 0;
+        caret-color: $cursor;
         color: $light-gray;
       }
     }
+  }
 
-    .show-pwd {
-      position: absolute;
-      right: 10px;
-      top: 7px;
-      font-size: 16px;
-      cursor: pointer;
-      user-select: none;
-      color: $dark-gray;
+  .svg-container {
+    display: inline-block;
+    padding: 6px 5px 6px 15px;
+    vertical-align: middle;
+    color: $dark-gray;
+  }
+
+  .title-container {
+    position: relative;
+
+    .title {
+      margin: 0 auto 40px auto;
+      font-size: 26px;
+      text-align: center;
+      font-weight: bold;
+      color: $light-gray;
     }
   }
+
+  .show-pwd {
+    position: absolute;
+    right: 10px;
+    top: 10px;
+    font-size: 16px;
+    cursor: pointer;
+    user-select: none;
+    color: $dark-gray;
+  }
+}
 </style>
