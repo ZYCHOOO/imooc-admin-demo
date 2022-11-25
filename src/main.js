a62180c22e31e213ed1ade03b37520d6c6dd6575
曾逸超 <zengyichao@digibms.com>
2022-11-25 16:13:38 +0800

    chore: element-plus引入

--- src/main.js
+++ src/main.js
@@ -2,5 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import installElementPlus from './plugins/element'
+// 导入 svgIcon
+import installIcons from '@/icons'
 
-createApp(App).use(store).use(router).mount('#app')
+import '@/styles/index.scss'
+
+const app = createApp(App)
+installElementPlus(app)
+installIcons(app)
+
+app
+  .use(store)
+  .use(router)
+  .mount('#app')
